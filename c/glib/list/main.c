a3bf76d128c27390987320238e268e21119cd63c
jxv <joevargas@hush.com>
2014-11-07 08:55:28 -0800

    add c/glib/list

---
+++ c/glib/list/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+
+
+
+void print_node(gpointer data, gpointer user_data)
+{
+	printf("%s", (const char *)data);
+}
+
+
+int main(int argc, const char **argv)
+{
+	GList *list = NULL;
+	list = g_list_append(list, "Hello,");
+	list = g_list_append(list, " ");
+	list = g_list_append(list, "World!\n");
+
+	g_list_foreach(list, print_node, NULL);
+
+	return EXIT_SUCCESS;
+}
